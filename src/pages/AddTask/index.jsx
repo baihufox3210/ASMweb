92ffe791b5d07ee279a1b34e6aba5cf48dfce4c2
baihu <baihu3210@gmail.com>
2026-01-04 13:08:06 +0800

    feat: 初始化 React 應用程式結構，包含核心組件、頁面及樣式。

---
+++ src/pages/AddTask/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddTask.css';
+
+const AddTask = () => {
+    return (
+        <div className="add-task-container">
+            <h1>新增任務</h1>
+        </div>
+    );
+};
+
+export default AddTask;
